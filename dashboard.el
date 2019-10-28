4b154b905fd23406e2321ac9e14e83fb91e7b5ba
Yasuyuki Ageishi <yasuyuki_ageishi@so-netmedia.jp>
2019-10-29 00:09:29 +0900

    set dashboard items

--- dashboard.el
+++ dashboard.el
@@ -5,4 +5,7 @@
   :config
   (dashboard-setup-startup-hook)
   (setq dashboard-set-heading-icons t)
-  (setq dashboard-set-file-icons t))
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-items '((recents . 10)
+			  (bookmarks . 5)
+			  (agenda . 5))))
