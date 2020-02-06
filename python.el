93de0ec8392a253bdd70c4c0a78ef52268be6a91
Yasuyuki Ageishi <yasuyuki_ageishi@so-netmedia.jp>
2020-02-06 16:37:03 +0900

    update python and web mode

--- python.el
+++ python.el
@@ -2,3 +2,11 @@
   :init
   (setq exec-path (append '("~/.asdf/bin" "~/.asdf/shims")
                           exec-path)))
+
+(use-package pipenv
+  :ensure t
+  :hook (python-mode . pipenv-mode)
+  :init
+  (setq pipenv-projectile-after-switch-function
+        #'pipenv-projectile-after-switch-extended))
+
