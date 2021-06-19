6f842d55f502810f00371144c4ba21e049e61e46
Y4suyuki <4ge15h1@gmail.com>
2021-06-20 00:51:11 +0900

    added mojor-mode-hydra

--- hydra.el
+++ hydra.el
@@ -14,3 +14,32 @@ zoom"
   ("n" org-narrow-to-subtree)
   ("w" widen)
   ("e" org-export-dispatch))
+
+(use-package major-mode-hydra
+  :ensure t
+  :bind
+  ("M-SPC" . major-mode-hydra))
+
+(major-mode-hydra-define org-mode nil
+  ("Tree"
+   (("n" org-narrow-to-subtree "narrow to subtree")
+    ("w" widen "widen"))
+   "Export"
+   (("e" org-export-dispatch))))
+
+(major-mode-hydra-define emacs-lisp-mode nil
+  ("Eval"
+   (("b" eval-buffer "buffer")
+    ("e" eval-defun "defun")
+    ("r" eval-region "region"))
+   "REPL"
+   (("I" ielm "ielm"))
+   "Test"
+   (("t" ert "prompt")
+    ("T" (ert t) "all")
+    ("F" (ert :failed) "failed"))
+   "Doc"
+   (("d" describe-foo-at-point "thing-at-pt")
+    ("f" describe-function "function")
+    ("v" describe-variable "variable")
+    ("i" info-lookup-symbol "info lookup"))))
