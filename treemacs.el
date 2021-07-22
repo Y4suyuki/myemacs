5e14f2613c885b7d81a74aabbbe826f10be57aca
Y4suyuki <4ge15h1@gmail.com>
2021-07-23 01:24:22 +0900

    treemacs all-the-icon-theme

--- treemacs.el
+++ treemacs.el
@@ -1,10 +1,13 @@
 (use-package treemacs
   :ensure t
   :defer t
+  :after (treemacs-all-the-icons)
   :bind
   (:map global-map
         ("M-0" . treemacs-select-window)
-        ("C-x t t" . treemacs)))
+        ("C-x t t" . treemacs))
+  :config
+  (treemacs-load-theme "all-the-icons"))
 
 (use-package treemacs-projectile
   :after (treemacs projectile)
@@ -14,6 +17,9 @@
   :after (treemacs dired)
   :ensure t
   :config (treemacs-icons-dired-mode))
+
+(use-package treemacs-all-the-icons
+  :ensure t)
 
 (use-package treemacs-magit
   :after (treemacs magit)
