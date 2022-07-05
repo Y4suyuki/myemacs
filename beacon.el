ef6a052931bc072efbcced2e89862310d011cf0f
Yasuyuki Ageishi <ageishi.yasuyuki@moneyforward.co.jp>
2022-07-05 13:35:00 +0900

    off beacon in org-tree-slide

--- beacon.el
+++ beacon.el
@@ -1,5 +1,10 @@
+(defun beacon-off ()
+  "beacon mode off"
+  (beacon-mode 0))
+
 (use-package beacon
   :ensure t
+  :hook (org-tree-slide-mode . beacon-off)
   :custom
   (beacon-color "#dfff00")
   :config
