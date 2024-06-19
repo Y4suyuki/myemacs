8acaaa980df135b3aefd6118c68dc20feeb336bb
y4suyuki <y4suyuki@protonmail.com>
2024-06-19 10:23:58 +0900

    update evil settings

--- evil.el
+++ evil.el
@@ -2,5 +2,8 @@
   :ensure t
   :config
   (evil-mode 1)
+  (evil-set-leader 'normal (kbd "SPC"))
+  (evil-define-key 'normal 'global (kbd "<leader>fs") 'save-buffer)
+  (evil-define-key 'normal 'global (kbd "<leader>SPC") 'major-mode-hydra)
   (define-key evil-normal-state-map (kbd "/") 'swiper)
   (evil-define-key 'normal 'python-mode-map (kbd "R") 'lsp-find-references))
